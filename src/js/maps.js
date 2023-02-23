66d54a35cbea9c5e21d65c360d4b566f5feaa488
baptisteRiviere <riviereb85@gmail.com>
2023-02-23 17:03:22 +0100

    init map

---
+++ src/js/maps.js
@@ -0,0 +1,8 @@
+console.log("ok");
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
