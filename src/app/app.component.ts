cdb8ad959842de0a00a54b7650d9231f7af81da8
Erzalt <Cooookies27@gmail.com>
2024-01-04 22:01:52 +0100

    add toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jeuxCarto';
+  items = [
+    {
+        label: 'Update',
+        icon: 'pi pi-refresh'
+    },
+    {
+        label: 'Delete',
+        icon: 'pi pi-times'
+    }
+];
 }
